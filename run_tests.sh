1e24da9b5f33f7fd1fb7f45c6d17756125d862ef
Martin Hruska <hruskamartin25@gmail.com>
2015-05-06 17:37:19 +0200

    upgrade

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,6 @@ do
 	for i in `ls /home/martin/forester/fa/sv-comp/c/$k`;
 	do
 		echo $i >> $3
-		timeout 100 $FAPATH/fa_build/sv_comp_run.py --trace $FAPATH/fa/sv-comp/witness/`basename $i`".xml" --time $i >> $3
+		timeout 100 $FAPATH/fa_build/sv_comp_run.py --false --trace $FAPATH/fa/sv-comp/witness/`basename $i`".xml" --time $i >> $3
 	done
 done
